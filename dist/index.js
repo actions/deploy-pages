01f6ae5af2334ea97ecf6fa1565431c37ecc8996
Jess Bees <toomanybees@github.com>
2022-03-31 12:05:36 -0400

    Recompile action after pulling in PR suggestions

--- dist/index.js
+++ dist/index.js
@@ -7473,8 +7473,7 @@ process.on('SIGINT', cancelHandler)
 process.on('SIGTERM', cancelHandler)
 
 // Main
-// Boolean inputs are stringified, but manually convert this input just in case that changes in the future.
-const emitTelemetry = core.getInput("emit_telemetry").toString();
+const emitTelemetry = core.getInput("emit_telemetry")
 if (emitTelemetry === "true") {
   __nccwpck_require__(9557)
 } else if (emitTelemetry === "false") {
