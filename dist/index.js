5c24c29b4db3485ef90f8ea73de1fbae0524b6fd
Jess Bees <hi@toomanybees.com>
2022-11-16 12:21:11 -0500

    report the env var

--- dist/index.js
+++ dist/index.js
@@ -6995,6 +6995,7 @@ class Deployment {
       }
       core.info(`Creating deployment with payload:\n${JSON.stringify(payload, null, '\t')}`)
       core.info(`Sending payload to: ${pagesDeployEndpoint}`)
+      core.info(`It should be sent do: ${process.env.GITHUB_API_URL}`)
       const response = await axios.post(pagesDeployEndpoint, payload, {
         headers: {
           Accept: 'application/vnd.github.v3+json',
