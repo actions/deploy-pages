60c66393978c81c0a132c3626670773deca457bb
James M. Greene <JamesMGreene@github.com>
2023-08-16 09:07:04 -0500

    Add explicit context tests for more coverage

---
+++ src/__tests__/internal/context.test.js
@@ -0,0 +1,38 @@
+const getContext = require('../../internal/context')
+
+describe('getContext', () => {
+  beforeEach(() => {
+    process.env.ACTIONS_RUNTIME_URL = 'http://my-url'
+    process.env.GITHUB_RUN_ID = '123'
+    process.env.ACTIONS_RUNTIME_TOKEN = 'a-token'
+    process.env.GITHUB_REPOSITORY = 'actions/is-awesome'
+    process.env.GITHUB_TOKEN = 'gha-token'
+    process.env.GITHUB_SHA = '123abc'
+    process.env.GITHUB_ACTOR = 'monalisa'
+    process.env.GITHUB_ACTION = '__monalisa/octocat'
+    process.env.GITHUB_ACTION_PATH = 'something'
+  })
+
+  it('succeeds with all environment variables set', () => {
+    const context = getContext()
+    expect(context).toEqual({
+      actionsId: '__monalisa/octocat',
+      artifactName: 'github-pages',
+      buildActor: 'monalisa',
+      buildVersion: '123abc',
+      githubApiUrl: 'https://api.github.com',
+      githubServerUrl: 'https://github.com',
+      githubToken: '',
+      isPreview: false,
+      repositoryNwo: 'actions/is-awesome',
+      runTimeToken: 'a-token',
+      runTimeUrl: 'http://my-url',
+      workflowRun: '123'
+    })
+  })
+
+  it('throws if there are missing variables', () => {
+    delete process.env.ACTIONS_RUNTIME_URL
+    expect(getContext).toThrow('runTimeUrl is undefined. Cannot continue.')
+  })
+})
